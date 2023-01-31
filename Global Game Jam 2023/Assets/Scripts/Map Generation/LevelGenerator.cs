b71fc81a18d76dc71f8aaab40a88d106fe4c9523
lorenleiro <thelooolren@gmail.com>
2023-01-31 23:14:36 +0100

    Basic root grow system

--- Global Game Jam 2023/Assets/Scripts/Map Generation/LevelGenerator.cs
+++ Global Game Jam 2023/Assets/Scripts/Map Generation/LevelGenerator.cs
@@ -1,15 +1,21 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using System.Collections.Generic;
+using System;
 
 public class LevelGenerator : MonoBehaviour
 {
+    /// <summary>
+    /// Invoked when the level finished loading. Returns the array containing level structure and the player spawn position.
+    /// </summary>
+    public static Action<int[,], TileCoord> LevelCreated;
+
     private Grid levelGrid;
     private Tilemap levelTilemap;
     private PlayerSpawner playerSpawner;
     private LevelExit levelExit;
-    // Bidimensional array containing all the tiles of a level
-    private int[,] level;
+    private TileCoord rootsStartTile;
+    private int[,] level;// Bidimensional array containing all the tiles of a level
 
     [Tooltip("Width of the level.")]
     [SerializeField] private int width;
@@ -53,6 +59,7 @@ public class LevelGenerator : MonoBehaviour
         CelullarAutomata(CASteps, false);
         CalculateLevelRegions();
         DrawTilemap();
+        LevelCreated?.Invoke(level, rootsStartTile);
     }
 
     /// <summary>
@@ -77,7 +84,7 @@ public class LevelGenerator : MonoBehaviour
                     if (x == levelMiddle)
                         level[x, y] = 0;
                     else
-                        level[x, y] = Random.Range(0, 100) < tileFillAmount ? 1 : 0;
+                        level[x, y] = UnityEngine.Random.Range(0, 100) < tileFillAmount ? 1 : 0;
                 }
             }
         }
@@ -222,8 +229,8 @@ public class LevelGenerator : MonoBehaviour
     private void CreateLevelDoors(List<Region> regions)
     {
         // We choose different regions if avaliable
-        int rndPlayerSpawnRegion = Random.Range(0, regions.Count);
-        int rndExitRegion = Random.Range(0, regions.Count);
+        int rndPlayerSpawnRegion = UnityEngine.Random.Range(0, regions.Count);
+        int rndExitRegion = UnityEngine.Random.Range(0, regions.Count);
 
         if (rndPlayerSpawnRegion == rndExitRegion && regions.Count > 1)
         {
@@ -239,7 +246,7 @@ public class LevelGenerator : MonoBehaviour
         // Search for a correct player spawn tile
         while (!spawnTileFound)
         {
-            int rndTile = Random.Range(0, regions[rndPlayerSpawnRegion].Tiles.Count);
+            int rndTile = UnityEngine.Random.Range(0, regions[rndPlayerSpawnRegion].Tiles.Count);
             TileCoord selectedTile = regions[rndPlayerSpawnRegion].Tiles[rndTile];
 
             if (level[selectedTile.xCoord, selectedTile.yCoord] == 0)
@@ -258,7 +265,7 @@ public class LevelGenerator : MonoBehaviour
         // Search for a correct player level exit tile
         while (!exitTileFound)
         {
-            int rndTile = Random.Range(0, regions[rndExitRegion].Tiles.Count);
+            int rndTile = UnityEngine.Random.Range(0, regions[rndExitRegion].Tiles.Count);
             TileCoord selectedTile = regions[rndExitRegion].Tiles[rndTile];
 
             if (level[selectedTile.xCoord, selectedTile.yCoord] == 0)
@@ -286,6 +293,7 @@ public class LevelGenerator : MonoBehaviour
         // Set the player spawner and level exit position on the newly created map.
         playerSpawner.transform.position = CoordToWorldPoint(spawnTile);
         levelExit.transform.position = CoordToWorldPoint(exitTile);
+        rootsStartTile = spawnTile;
     }
 
     /// <summary>
@@ -599,5 +607,10 @@ public class LevelGenerator : MonoBehaviour
     private void GenerateNewRandomMap()
     {
         CreateNewLevel();
+    }
+
+    public void UpdateTile(TileCoord tile, Tile newTile)
+    {
+        levelTilemap.SetTile(new Vector3Int(tile.xCoord, tile.yCoord), newTile);
     }
 }
