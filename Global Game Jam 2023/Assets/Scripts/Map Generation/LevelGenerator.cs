1bdf92ecd03056c20772b597e04ce021df4d9e6d
lorenleiro <thelooolren@gmail.com>
2023-01-31 16:25:17 +0100

    Procedural generation with passages between regions and player spawn/level exit

--- Global Game Jam 2023/Assets/Scripts/Map Generation/LevelGenerator.cs
+++ Global Game Jam 2023/Assets/Scripts/Map Generation/LevelGenerator.cs
@@ -6,6 +6,8 @@ public class LevelGenerator : MonoBehaviour
 {
     private Grid levelGrid;
     private Tilemap levelTilemap;
+    private PlayerSpawner playerSpawner;
+    private LevelExit levelExit;
     // Bidimensional array containing all the tiles of a level
     private int[,] level;
 
@@ -21,16 +23,29 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] private int CASteps = 50;
     [Tooltip("Minimun number of tiles for a region to exits.")]
     [SerializeField] private int regionSizeThreshold = 50;
+    [Tooltip("Size of the passages created between regions.")]
+    [Range(1, 5)]
+    [SerializeField] private int passageCreationRadius = 1;
     [Tooltip("Seed used for map generation. Leave empty for a random seed.")]
     [SerializeField] private string mapSeed;
+    [Tooltip("Generated seed, for visualization purposes only.")]
+    [SerializeField] private int generatedSeed;
     [SerializeField] private Tile tileTest;
 
     private void Awake()
     {
+        playerSpawner = FindObjectOfType<PlayerSpawner>();
+        levelExit = FindObjectOfType<LevelExit>();
         levelGrid = GetComponentInChildren<Grid>();
         levelTilemap = GetComponentInChildren<Tilemap>();
 
-        SeedGenerator.GenerateSeed(mapSeed);
+        CreateNewLevel();
+    }
+
+    private void CreateNewLevel()
+    {
+        levelTilemap.ClearAllTiles();
+        generatedSeed = SeedGenerator.GenerateSeed(mapSeed);
         InitializeRandomTilemap();
         CelullarAutomata(CASteps, false);
         CalculateLevelRegions();
@@ -169,16 +184,16 @@ public class LevelGenerator : MonoBehaviour
     }
 
     /// <summary>
-    /// 
+    /// Calculates all the regions inside this level, removes the smaller ones and sorts them from bigger to smaller. Finally, connects all of them together.
     /// </summary>
     private void CalculateLevelRegions()
     {
-        List<List<TileCoord>> regions = GetAllRegions(0);
-        List<Region> finalRegions = new List<Region>();
+        List<List<TileCoord>> regions = GetAllRegions(0);// We get a list with all the walkable regions
+        List<Region> finalRegions = new List<Region>(); // List containing the surviving regions
 
         foreach (List<TileCoord> region in regions)
         {
-            tileTest.color = Random.ColorHSV();
+            // Remove the smaller regions
             if (region.Count < regionSizeThreshold)
             {
                 foreach (TileCoord tile in region)
@@ -189,11 +204,106 @@ public class LevelGenerator : MonoBehaviour
             else
                 finalRegions.Add(new Region(region, level));
         }
+        finalRegions.Sort();
+        finalRegions[0].IsMainRegion = true; // The biggest one is the main region.
+        finalRegions[0].IsAccesibleFromMainRegion = true; // And it is also accesible to itself.
+
+        CreateLevelDoors(finalRegions);
         ConnectAllRegions(finalRegions);
     }
 
-    private void ConnectAllRegions(List<Region> regions)
+    /// <summary>
+    /// Creates the player spawn position and the level exit position.
+    /// </summary>
+    /// <param name="regions">List containing all the regions of the map.</param>
+    private void CreateLevelDoors(List<Region> regions)
     {
+        // We choose different regions if avaliable
+        int rndPlayerSpawnRegion = Random.Range(0, regions.Count);
+        int rndExitRegion = Random.Range(0, regions.Count);
+
+        if (rndPlayerSpawnRegion == rndExitRegion && regions.Count > 1)
+        {
+            CreateLevelDoors(regions);
+            return;
+        }
+
+        TileCoord spawnTile = new(); // Tile used for positioning the player spawner
+        TileCoord exitTile = new(); // Tile used for positioning the level exit
+        bool spawnTileFound = false;
+        bool exitTileFound = false;
+
+        // Search for a correct player spawn tile
+        while (!spawnTileFound)
+        {
+            int rndTile = Random.Range(0, regions[rndPlayerSpawnRegion].Tiles.Count);
+            TileCoord selectedTile = regions[rndPlayerSpawnRegion].Tiles[rndTile];
+
+            if (level[selectedTile.xCoord, selectedTile.yCoord] == 0)
+            {
+                foreach (TileCoord tile in regions[rndPlayerSpawnRegion].Tiles)
+                {
+                    if (selectedTile.xCoord == tile.xCoord && selectedTile.yCoord == tile.yCoord)
+                    {
+                        spawnTile = tile;
+                        spawnTileFound = true;
+                    }
+                }
+            }
+        }
+
+        // Search for a correct player level exit tile
+        while (!exitTileFound)
+        {
+            int rndTile = Random.Range(0, regions[rndExitRegion].Tiles.Count);
+            TileCoord selectedTile = regions[rndExitRegion].Tiles[rndTile];
+
+            if (level[selectedTile.xCoord, selectedTile.yCoord] == 0)
+            {
+                foreach (TileCoord tile in regions[rndExitRegion].Tiles)
+                {
+                    if (selectedTile.xCoord == tile.xCoord && selectedTile.yCoord == tile.yCoord)
+                    {
+                        exitTile = tile;
+                        exitTileFound = true;
+
+                    }
+                }
+            }
+        }
+        // Set the player spawner and level exit position on the newly created map.
+        playerSpawner.transform.position = CoordToWorldPoint(spawnTile);
+        levelExit.transform.position = CoordToWorldPoint(exitTile);
+    }
+
+    /// <summary>
+    /// Connects all regions together.
+    /// </summary>
+    /// <param name="regions">List containing the regions to connect.</param>
+    /// <param name="forceAccessFromMainRegion">Force the access to the main region.</param>
+    private void ConnectAllRegions(List<Region> regions, bool forceAccessFromMainRegion = false)
+    {
+        // All this regions aren't accesible from the main region
+        List<Region> regionsA = new List<Region>();
+        // All this regions are accesible from the main region
+        List<Region> regionsB = new List<Region>();
+
+        if (forceAccessFromMainRegion)
+        {
+            foreach (Region r in regions)
+            {
+                if (r.IsAccesibleFromMainRegion)
+                    regionsB.Add(r);
+                else
+                    regionsA.Add(r);
+            }
+        }
+        else
+        {
+            regionsA = regions;
+            regionsB = regions;
+        }
+
         TileCoord closestTileA = new TileCoord();
         TileCoord closestTileB = new TileCoord();
         Region closestRegionA = new Region();
@@ -201,13 +311,19 @@ public class LevelGenerator : MonoBehaviour
         bool connectionFound = false;
         int closestDistance = 0;
 
-        foreach (Region regionA in regions)
+        foreach (Region regionA in regionsA)
         {
-            connectionFound = false;
-
-            foreach (Region regionB in regions)
+            if (!forceAccessFromMainRegion)
             {
-                if (regionA == regionB)
+                connectionFound = false;
+
+                if (regionA.ConnectedRegions.Count > 0)
+                    continue;
+            }
+
+            foreach (Region regionB in regionsB)
+            {
+                if (regionA == regionB || regionA.IsConnected(regionB))
                     continue;
 
                 if (regionA.IsConnected(regionB))
@@ -215,7 +331,6 @@ public class LevelGenerator : MonoBehaviour
                     connectionFound = false;
                     break;
                 }
-
 
                 for (int tileIndexA = 0; tileIndexA < regionA.BorderTiles.Count; tileIndexA++)
                 {
@@ -236,25 +351,124 @@ public class LevelGenerator : MonoBehaviour
                         }
                     }
                 }
-                if (connectionFound)
-                {
-                    CreatePassage(closestRegionA, closestRegionB, closestTileA, closestTileB);
-                    continue;
-                }
-
             }
+            if (connectionFound && !forceAccessFromMainRegion)
+                CreatePassage(closestRegionA, closestRegionB, closestTileA, closestTileB);
         }
+        if (connectionFound && forceAccessFromMainRegion)
+        {
+            CreatePassage(closestRegionA, closestRegionB, closestTileA, closestTileB);
+
+        }
+
+        if (!forceAccessFromMainRegion)
+            ConnectAllRegions(regions, true);
     }
 
+    /// <summary>
+    /// Creates passages between regions.
+    /// </summary>
+    /// <param name="regionA">Start region.</param>
+    /// <param name="regionB">End regions.</param>
+    /// <param name="tileA">Start tile.</param>
+    /// <param name="tileB">End tile.</param>
     private void CreatePassage(Region regionA, Region regionB, TileCoord tileA, TileCoord tileB)
     {
         Region.ConnectRegions(regionA, regionB);
         //Debug.DrawLine(CoordToWorldPoint(tileA), CoordToWorldPoint(tileB), Color.green, 100f);
+
+        List<TileCoord> line = CreateLine(tileA, tileB);
+
+        foreach (TileCoord tile in line)
+            RemoveCircle(tile, passageCreationRadius);
     }
 
+    /// <summary>
+    /// Removes the tiles around the specified tile coordinates.
+    /// </summary>
+    /// <param name="coord">Middle tile.</param>
+    /// <param name="radius">Radious to remove.</param>
+    private void RemoveCircle(TileCoord coord, int radius)
+    {
+        for (int x = -radius; x <= radius; x++)
+        {
+            for (int y = -radius; y <= radius; y++)
+            {
+                // If we are inside the circle
+                if (x * x + y * y <= radius * radius)
+                {
+                    int removeX = coord.xCoord + x;
+                    int removeY = coord.yCoord + y;
+
+                    if (TileInsideLevel(removeX, removeY))
+                        level[removeX, removeY] = 0;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Calculates a line between two Tile Coordinates.
+    /// </summary>
+    /// <param name="from">Start tile.</param>
+    /// <param name="to">End tile.</param>
+    /// <returns>List containing all the tiles coordinates inside the level.</returns>
+    private List<TileCoord> CreateLine(TileCoord from, TileCoord to)
+    {
+        List<TileCoord> line = new List<TileCoord>();
+        int x = from.xCoord;
+        int y = from.yCoord;
+
+        int dx = to.xCoord - from.xCoord;
+        int dy = to.yCoord - from.yCoord;
+
+        bool inverted = false;
+        int step = (int)Mathf.Sign(dx);
+        int gradientStep = (int)Mathf.Sign(dy);
+
+        int longest = Mathf.Abs(dx);
+        int shortest = Mathf.Abs(dy);
+
+        if (longest < shortest)
+        {
+            inverted = true;
+            longest = Mathf.Abs(dy);
+            shortest = Mathf.Abs(dx);
+
+            step = (int)Mathf.Sign(dy);
+            gradientStep = (int)Mathf.Sign(dx);
+        }
+        int gradientAccumulation = longest / 2;
+        for (int i = 0; i < longest; i++)
+        {
+            line.Add(new TileCoord(x, y));
+
+            if (inverted)
+                y += step;
+            else
+                x += step;
+
+            gradientAccumulation += shortest;
+            if (gradientAccumulation >= longest)
+            {
+                if (inverted)
+                    x += gradientStep;
+                else
+                    y += gradientStep;
+            }
+        }
+        return line;
+    }
+
+    /// <summary>
+    /// Converts a tile coordinate to a world Vector3.
+    /// </summary>
+    /// <param name="coordinate">Tile coordinate.</param>
+    /// <returns>Vector3 containing the world position.</returns>
     private Vector3 CoordToWorldPoint(TileCoord coordinate)
     {
         return levelTilemap.CellToWorld(new Vector3Int(coordinate.xCoord, coordinate.yCoord));
+        //return new Vector3(-width / 2 + 0.5f + coordinate.xCoord, 2,-height / 2 + 0.5f + coordinate.yCoord);
     }
 
     /// <summary>
@@ -293,7 +507,7 @@ public class LevelGenerator : MonoBehaviour
     }
 
     /// <summary>
-    /// Flodd fill algorithm. Checks for the same tile near the initial tile and creates a list containing all the coordinates of each tile.
+    /// Flood fill algorithm. Checks for the same tile near the initial tile and creates a list containing all the coordinates of each tile.
     /// </summary>
     /// <param name="startX">Tile X coordinate.</param>
     /// <param name="startY">Tile Y coordinate.</param>
@@ -359,11 +573,6 @@ public class LevelGenerator : MonoBehaviour
     [ContextMenu("New level")]
     private void GenerateNewRandomMap()
     {
-        levelTilemap.ClearAllTiles();
-        SeedGenerator.GenerateSeed(mapSeed);
-        InitializeRandomTilemap();
-        CelullarAutomata(CASteps, true);
-        CalculateLevelRegions();
-        DrawTilemap();
+        CreateNewLevel();
     }
 }
