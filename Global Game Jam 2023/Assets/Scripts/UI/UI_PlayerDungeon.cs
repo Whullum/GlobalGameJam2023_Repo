65c6dc7d59ef8ad9d5eb02712349817929afc737
Will <55513273+Whullum@users.noreply.github.com>
2023-01-31 19:59:05 -0500

    created ui mockups and prefabs for future use

---
+++ Global Game Jam 2023/Assets/Scripts/UI/UI_PlayerDungeon.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UI_PlayerDungeon : MonoBehaviour
+{
+    public TextMeshProUGUI seedsCounterText;
+    public TextMeshProUGUI weaponText;
+    public TextMeshProUGUI abilityText;
+    public TextMeshProUGUI abilityCountdownText;
+    public TextMeshProUGUI levelText;
+    public TextMeshProUGUI levelTimerText;
+}
---
+++ Global Game Jam 2023/Assets/UI_HubWeaponShopBheavior.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class UI_HubWeaponShopBheavior : MonoBehaviour
+{
+    public Button buyButton;
+    public Button exitButton;
+
+    public TextMeshProUGUI seedsCounter;
+    public TextMeshProUGUI dialogueNPC;
+
+    private void Start()
+    {
+        // Initialize shop values
+    }
+
+    public void BuyItem()
+    {
+
+    }
+
+    public void ChangeNPCDialogue()
+    {
+
+    }
+
+    public void ExitShop()
+    {
+
+    }
+}
