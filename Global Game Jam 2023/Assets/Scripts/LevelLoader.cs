2c914e27a1d8c78f08a4b66b51005436450acb09
Will <55513273+Whullum@users.noreply.github.com>
2023-02-03 23:59:58 -0500

    fixed rule tile collider bug

--- Global Game Jam 2023/Assets/Scripts/LevelLoader.cs
+++ Global Game Jam 2023/Assets/Scripts/LevelLoader.cs
@@ -8,6 +8,7 @@ public class LevelLoader : MonoBehaviour
     public void LoadLevel(string levelName)
     {
         SceneManager.LoadScene(levelName);
+        Time.timeScale = 1;
     }
 
     public static void RestartGame() => SceneManager.LoadScene("UIMockups");
